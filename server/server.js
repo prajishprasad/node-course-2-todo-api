37c6fbda2bc3ffb0506d0366cd4204e90c587d23
prajishprasad <prajish.87@gmail.com>
2018-12-25 17:50:29 +0530

    Add GET /todos:id

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo.js');
 const {User} = require('./models/user.js');
 
+const {ObjectID} = require('mongodb');
+
 const app = express();
 
 //Middleware
@@ -22,6 +24,26 @@ app.post('/todos', (req,res)=>{
     });
 });
 
+app.get('/todos', (req,res) => {
+    Todo.find().then((todos) => {
+        res.send({todos}); //Passing this as an object is better as it allows you to add extra fields later on
+    }, (err) => {
+        res.status(400).send(e);
+    })
+});
+
+app.get('/todos/:id', (req,res) => {
+    const id = req.params.id;
+    if(!ObjectID.isValid(id))
+        res.send('Invalid ID');
+    Todo.findById(id).then((todo)=>{
+        if(!todo){
+            res.status(404).send('TODO does not exist');
+        }
+        res.send(todo);
+    });
+    //res.send(req.params);
+});
 
 app.listen('3000', () =>{
     console.log('Started on port 3000');
